f7c2d4c88630037cfacbff325862f1a2eafd44b1
daniel <danik350@gmx.at>
2024-04-11 10:20:39 +0200

    jump

--- Test/Assets/MovementBehaviour.cs
+++ Test/Assets/MovementBehaviour.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MovementBehaviour : MonoBehaviour
 {
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,5 +17,10 @@ public class MovementBehaviour : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         transform.Translate(new Vector3(x, 0, z) * Time.deltaTime * 5f);
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        }
     }
 }
